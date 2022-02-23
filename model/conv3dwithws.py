6b40bbd7693f332fa7fb24d4a65f55aa7e527faf
bramantyo <bramantyo.supriyatno@bccn-berlin.de>
2022-02-23 11:25:29 +0100

    conv3dws update

--- model/conv3dwithws.py
+++ model/conv3dwithws.py
@@ -1,23 +1,15 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Conv3d
+from tensorflow.keras.layers import Conv3D
 
 
-class Conv3dWithWeightStandardization(Conv3d):
-    def standardize_weight(self, weights, eps=1e-6):
-        mean = tf.math.reduce_mean(weights, axis=(0, 1, 2), keepdims=True)
-        var = tf.math.reduce_variance(weights, axis=(0, 1, 2), keepdims=True)
-        fan_in = np.prod(weight.shape[:-1])
-        gain = self.add_weight(
-            name="gain",
-            shape=(weight.shape[-1],),
-            initializer="ones",
-            trainable=True,
-            dtype=self.dtype,
-        )
-        scale = (
-            tf.math.rsqrt(
-                tf.math.maximum(var * fan_in, tf.convert_to_tensor(eps, dtype=self.dtype))
-            )
-            * gain
-        )
-        return weight * scale - (mean * scale)
+class Conv3DWithWeightStandardization(Conv3D):
+    def standardize_weight(self, kernel, std_tol=1e-6):
+        mean = tf.reduce_mean(kernel, axis=(0,1,2,3), keepdims=True)
+        std = tf.math.reduce_std(kernel, axis=(0,1,2,3), keepdims=True)
+        std = tf.math.maximum(std, std_tol)
+
+        return (kernel-mean) / std
+
+    def call(self, inputs):
+        self.kernel.assign(self.standardize_weight(self.kernel))
+        return super().call(inputs)
